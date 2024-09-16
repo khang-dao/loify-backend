4b47a818c1a08eefc1a17b5f6db57b372bc57cc2
khang-dao <86513733+khang-dao@users.noreply.github.com>
2024-09-16 14:02:56 +1000

    Setup/basic mvc (#1)
    * chore: update gitignore
    
    * setup: boostrap sprint boot app
    
    * feat: setup basic controller & service

---
+++ src/main/java/cloud/loify/controller/spotify/SpotifyController.java
@@ -0,0 +1,110 @@
+package com.example.demo.controller.spotify;
+
+import com.example.demo.dto.AddTracksRequestDTO;
+import com.example.demo.dto.CreatePlaylistRequestDTO;
+import com.example.demo.dto.TrackNamesDTO;
+import com.example.demo.service.SpotifyService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.result.view.RedirectView;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class SpotifyController {
+
+    private final SpotifyService spotifyService;
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    public SpotifyController(SpotifyService spotifyService, OAuth2AuthorizedClientService authorizedClientService){
+        this.spotifyService = spotifyService;
+        this.authorizedClientService = authorizedClientService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello world! :)";
+    }
+
+    @GetMapping("/spotify/login")
+    public ResponseEntity homeSecured() {
+        final String redirectUrl = "https://accounts.spotify.com/authorize?response_type=code&client_id=a8868f0f7a4f4a8885835375ff9ca242&scope=user-read-private%20user-read-email%20playlist-modify-public%20playlist-modify-private&state=gA9N5_ldRbWUO3YXklHrhnJ3XvYuO61nt7bsjPkw2cc%3D&redirect_uri=http://localhost:8080/api/spotify/login";
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("redirectUrl", redirectUrl);
+
+        return ResponseEntity
+                .status(HttpStatus.OK) // Status 200 OK
+                .body(responseBody); // Response body as JSON
+    }
+
+    @Deprecated
+    @GetMapping("/api/spotify/login")
+    public void getAuthCode(@AuthenticationPrincipal OAuth2User principal) {
+        spotifyService.updateRequestHeadersWithAuthToken(principal);
+    }
+
+    @Deprecated
+    @GetMapping("/login/oauth2/token/spotify")
+    public String handleCallback(@RequestParam("code") String code) {
+        this.spotifyService.authCode = code;
+        System.out.println("Authorization code received: " + this.spotifyService.authCode);
+        return "call back C:";
+    }
+
+    @Deprecated
+    @PostMapping("/api/spotify/auth/token")
+    public void setAuthToken() {
+        spotifyService.setAuthToken();
+    }
+
+    @GetMapping("/api/spotify/users/{username}/playlists")
+    public void getAllPlaylistsByUserId(@PathVariable String username) {
+        System.out.println(spotifyService.getAllPlaylistsByUserId(username));
+    }
+
+    @GetMapping("/api/spotify/playlists/{playlistId}")
+    public void getAllTrackNamesInPlaylist(@PathVariable String playlistId) {
+        System.out.println(spotifyService.getAllTrackNamesInPlaylist(playlistId));
+    }
+
+//    @GetMapping("/api/spotify/playlists/{playlistId}")
+//    public void getAllTrackIdsInPlaylist(@PathVariable String playlistId) {
+//        System.out.println(spotifyService.getAllTrackIdsInPlaylist(playlistId));
+//    }
+
+    @GetMapping("/api/spotify/tracks/{trackName}")
+    public void getFirstTrackIdByTrackName(@PathVariable String trackName) {
+        System.out.println(spotifyService.getFirstTrackIdByTrackName(trackName));
+    }
+
+     @PostMapping("/api/spotify/users/{username}/playlists")
+    public void createPlaylist(@PathVariable String username, @RequestBody CreatePlaylistRequestDTO requestBody) {
+        spotifyService.createPlaylist(username, requestBody);
+    }
+
+    @PostMapping("/api/spotify/playlists/{playlistId}/tracks")
+    public void addTracksToPlaylist(@PathVariable String playlistId, @RequestBody AddTracksRequestDTO requestBody) {
+        spotifyService.addTracksToPlaylist(playlistId, requestBody);
+    }
+
+    @PostMapping("/api/spotify/tracks/loify")
+    public List<String> getLoifyedTracks(@RequestBody TrackNamesDTO requestBody) {
+        return spotifyService.getLoifyedTracks(requestBody);
+    }
+
+
+
+
+}
---
+++ src/main/java/cloud/loify/service/SpotifyService.java
@@ -0,0 +1,254 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.ModelAndView;
+import reactor.core.publisher.Mono;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@Service
+public class SpotifyService {
+
+    private final WebClient.Builder webClientBuilder;
+    private static final String BASE_URL = "https://api.spotify.com";
+    private static final String TOKEN_URL = "https://accounts.spotify.com/api/token";
+
+    @Value("${spotify.client_id}")
+    private String clientId;
+    @Value("${spotify.client_secret}")
+    private String clientSecret;
+    @Value("${spotify.grant_type}")
+    private String grantType;
+    @Value("${spotify.scopes}")
+    private String scopes;
+    @Value("${spotify.redirect_uri}")
+    private String redirectUri;
+
+    private WebClient webClient;
+    private String authToken;
+    public String authCode;
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    public SpotifyService(WebClient.Builder webClientBuilder, OAuth2AuthorizedClientService authorizedClientService) {
+        this.webClientBuilder = webClientBuilder;
+        this.webClient = webClientBuilder.baseUrl(BASE_URL).build();
+
+        this.authorizedClientService = authorizedClientService;
+    }
+
+
+
+    public void updateRequestHeadersWithAuthToken(@AuthenticationPrincipal OAuth2User principal) {
+        OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient("spotify", principal.getName());
+        String accessToken = client.getAccessToken().getTokenValue();
+        String refreshToken = client.getRefreshToken().getTokenValue();
+
+
+        this.webClient = this.webClientBuilder
+                .filter(ExchangeFilterFunction.ofRequestProcessor(request -> {
+                    ClientRequest updatedRequest = ClientRequest.from(request)
+                            .header("Authorization", "Bearer " + accessToken)
+                            .build();
+                    return Mono.just(updatedRequest);
+                }))
+                .build();
+
+        System.out.println("AUTHENTICATED :) happy coding");
+        System.out.println("accessToken: " + accessToken);
+        System.out.println("refreshToken: " + refreshToken);
+    }
+
+
+
+    @Deprecated
+    private void updateWebClientWithToken() {
+        this.webClient = this.webClientBuilder
+            .filter(ExchangeFilterFunction.ofRequestProcessor(request -> {
+                ClientRequest updatedRequest = ClientRequest.from(request)
+                    .header("Authorization", "Bearer " + this.authToken)
+                    .build();
+                return Mono.just(updatedRequest);
+            }))
+            .build();
+        System.out.println("AUTHENTICATED :) happy coding");
+        System.out.println("code: " + this.authCode);
+        System.out.println("token: " + this.authToken);
+    }
+
+    @Deprecated
+    public ModelAndView getAuthCode() {
+        System.out.println("scopes: " + this.scopes);
+
+        String encodedScopes = URLEncoder.encode(scopes, StandardCharsets.UTF_8);
+        String url = String.format(
+                "https://accounts.spotify.com/authorize?response_type=code&client_id=%s&scope=%s&redirect_uri=%s",
+                clientId,
+                encodedScopes,
+                redirectUri
+        );
+
+        System.out.println("Redirect URL: " + url);
+        return new ModelAndView("redirect:" + url);
+    }
+
+    @Deprecated
+    public void setAuthToken() {
+        SpotifyClientCredentialsDTO credentialsDTO = new SpotifyClientCredentialsDTO(
+                "authorization_code",       // grant_type
+                this.clientId,                   // client_id
+                this.clientSecret,               // client_secret
+                this.redirectUri,                // redirectUri
+                this.authCode                    // authCode
+        );
+
+        SpotifyAuthTokenResponseDTO response = this.webClient.post()
+            .uri(TOKEN_URL)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .bodyValue(credentialsDTO.toString())
+            .retrieve()
+            .bodyToMono(SpotifyAuthTokenResponseDTO.class)
+            .block();
+
+        this.authToken = response.access_token();
+        this.updateWebClientWithToken();
+    }
+
+    public List<String> getAllPlaylistsByUserId(String userId) {
+        return this.webClient.get()
+                .uri("/v1/users/" + userId + "/playlists")
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(JSONObject::new)
+                .map(jsonObject -> {
+                    JSONArray items = jsonObject.getJSONArray("items");
+                    List<String> playlistIds = new ArrayList<>();
+                    for (int i = 0; i < items.length(); i++) {
+                        JSONObject item = items.getJSONObject(i);
+                        playlistIds.add(item.getString("id"));
+                    }
+                    return playlistIds;
+                })
+                .block();
+    }
+
+    public List<String>  getAllTrackNamesInPlaylist(String playlistId) {
+        JSONArray tracks =  this.webClient.get()
+                .uri("/v1/playlists/" + playlistId)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(JSONObject::new)
+                .map(response -> response.getJSONObject("tracks").getJSONArray("items"))
+                .block();
+
+        Stream<JSONObject> tracksStream = IntStream.range(0, tracks.length()).mapToObj(tracks::getJSONObject);
+        List<String> trackNames = tracksStream.map(t -> t.getJSONObject("track").getString("name")).collect(Collectors.toList());
+
+        return trackNames;
+    }
+
+//    public List<String>  getAllTrackIdsInPlaylist(String playlistId) {
+//        JSONArray tracks =  this.webClient.get()
+//                .uri("/v1/playlists/" + playlistId)
+//                .retrieve()
+//                .bodyToMono(String.class)
+//                .map(JSONObject::new)
+//                .map(response -> response.getJSONObject("tracks").getJSONArray("items"))
+//                .block();
+//
+//        Stream<JSONObject> tracksStream = IntStream.range(0, tracks.length()).mapToObj(tracks::getJSONObject);
+//        List<String> trackNames = tracksStream.map(t -> t.getJSONObject("track").getString("id")).collect(Collectors.toList());
+//        return trackNames;
+//    }
+
+
+    public String getFirstTrackIdByTrackName(String trackName) {
+        try {
+            String trackId = this.webClient.get()
+                    .uri("/v1/search?q=track:" + trackName + "&type=track&limit=1")
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .map(JSONObject::new)
+                    .map(response -> {
+                                System.out.println(response);
+                                return response.getJSONObject("tracks").getJSONArray("items").getJSONObject(0).getString("id");
+                            }
+                    )
+                    .block();
+            return trackId;
+        }
+        catch (JSONException err) {
+            System.out.println("Song could not be Loify-ed: " + err);
+        }
+        return null;
+    }
+
+    public void createPlaylist(String username, CreatePlaylistRequestDTO requestBody) {
+        CreatePlaylistResponseDTO responseBody = this.webClient.post()
+                .uri("v1/users/" + username + "/playlists")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(CreatePlaylistResponseDTO.class)
+                .block();
+
+        System.out.println("Playlist Created: " + responseBody);
+    }
+
+    public void addTracksToPlaylist(String playlistId, AddTracksRequestDTO requestBody) {
+        System.out.println(requestBody);
+        String responseBody = this.webClient.post()
+                .uri("v1/playlists/" + playlistId + "/tracks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println("Tracks added to Playlist !");
+    }
+
+    private String loifyPlaylistName(String playlistName){
+        return "loify - " + playlistName;
+    }
+
+    private String loifyTrackName(String trackName){
+        return trackName + " lofi";
+    }
+
+    public List<String> getLoifyedTracks(TrackNamesDTO requestBody) {
+        List<String> trackNames = requestBody.trackNames();
+        Stream<String> loifyedTrackNames = trackNames.stream().map(this::loifyTrackName);
+        return loifyedTrackNames
+                .map(this::getFirstTrackIdByTrackName)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+
+
+
+    public void addCustomImageToPlaylist(String userId) {
+
+    }
+}
