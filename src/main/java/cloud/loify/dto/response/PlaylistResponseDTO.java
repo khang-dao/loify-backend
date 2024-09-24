e85de37733eb7a8050b26e48b28988c18cb9c53e
khang-dao <86513733+khang-dao@users.noreply.github.com>
2024-09-24 16:11:12 +1000

    feat: generate loify playlist cover image (#8)
    * chore: add todo for future ref
    
    * feat: generate loify playlist cover image

--- src/main/java/cloud/loify/dto/response/PlaylistResponseDTO.java
+++ src/main/java/cloud/loify/dto/response/PlaylistResponseDTO.java
@@ -1,6 +1,9 @@
 package cloud.loify.dto.response;
 
+import cloud.loify.dto.common.CoverImageDTO;
 import cloud.loify.dto.track.TracksDTO;
 
+import java.util.List;
 
-public record PlaylistResponseDTO(String href, String id, String name, TracksDTO tracks) {}
+
+public record PlaylistResponseDTO(String href, String id, String name, TracksDTO tracks, List<CoverImageDTO> images) {}
