6e75cc95be353cda5d7d694cc9be446ca38116a7
khang-dao <khangcs@outlook.com>
2024-11-06 13:45:02 +1100

    feat: implement method to standardize track (song) names

--- src/main/java/cloud/loify/packages/utils/StringUtils.java
+++ src/main/java/cloud/loify/packages/utils/StringUtils.java
@@ -2,15 +2,19 @@ package cloud.loify.packages.utils;
 
 public class StringUtils {
 
-    public static String customizePlaylistName(String playlistName, String genre){
+    public static String customizePlaylistName(String playlistName, String genre) {
         return "loify (" + genre.toLowerCase() + ") - " + playlistName + " 🍃";
     }
 
-    public static String customizePlaylistDescription(String playlistName, String genre){
+    public static String customizePlaylistDescription(String playlistName, String genre) {
         return "a " + genre.toLowerCase() + " version of playlist: " + playlistName;
     }
 
-    public static String customizeTrackName(String trackName, String genre){
-        return trackName + " " + genre.toLowerCase();
+    public static String customizeTrackName(String trackName, String genre) {
+        return standardizeTrackName(trackName) + " " + genre.toLowerCase();
+    }
+
+    private static String standardizeTrackName(String trackName) {
+        return trackName.toLowerCase().replaceAll("\\(.*?\\)", "").trim();
     }
 }
